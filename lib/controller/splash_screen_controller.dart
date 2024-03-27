bba7815fb2413e91f27ea476164e4e7bbefa4927
Vijay Bheda <vijayb@thirdrocktechkno.com>
2024-03-27 17:11:35 +0530

    - lazyPut GlobalConfigController

--- lib/controller/splash_screen_controller.dart
+++ lib/controller/splash_screen_controller.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:pverify/controller/global_config_controller.dart';
 import 'package:pverify/ui/login_screen.dart';
 
 class SplashScreenController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    Get.lazyPut(() => GlobalConfigController(), fenix: true);
     Future.delayed(const Duration(seconds: 0)).then((value) async {
       Get.offAll(() => const LoginScreen());
     });
