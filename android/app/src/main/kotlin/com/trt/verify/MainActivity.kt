f50d1c299cee4729642e98e30cbbdc1c5004b4c0
Vijay Bheda <vijayb@thirdrocktechkno.com>
2024-06-11 15:42:39 +0530

    - scan barcode MainActivity

--- android/app/src/main/kotlin/com/trt/verify/MainActivity.kt
+++ android/app/src/main/kotlin/com/trt/verify/MainActivity.kt
@@ -1,43 +1,37 @@
 package com.trt.verify
 
-import android.os.Bundle
+import android.content.Intent
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import com.google.zxing.integration.android.IntentIntegrator
-import com.google.zxing.integration.android.IntentResult
-import android.content.Intent
+
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "com.example.barcode_scan"
     private var result: MethodChannel.Result? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-                call, result ->
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             if (call.method == "scanBarcode") {
-                setResultHandler(res)
-                scanBarcode(result)
+                setResultHandler(result)
+                initiateScan()
             } else {
                 result.notImplemented()
             }
         }
     }
 
-    private fun setResultHandler(res: MethodChannel.Result) { result = res
-        // Set the result handler in a method to avoid scope issues
-        }
-
-    private fun scanBarcode(result: MethodChannel.Result) {
-        initiateScan()
-        //IntentIntegrator(this).initiateScan()
+    private fun setResultHandler(res: MethodChannel.Result) {
+        result = res
     }
 
     private fun initiateScan() {
         val integrator = IntentIntegrator(this)
         integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
         integrator.setPrompt("Scan a barcode")
-        integrator.setCameraId(0)  // Use a specific camera of the device
+        integrator.setCameraId(0)
         integrator.setBeepEnabled(true)
         integrator.setBarcodeImageEnabled(true)
         integrator.initiateScan()
@@ -48,7 +42,6 @@ class MainActivity : FlutterActivity() {
         val scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data) as IntentResult
         if (scanResult != null) {
             if (scanResult.contents == null) {
-
                 result?.success("")
             } else {
                 result?.success(scanResult.contents)
